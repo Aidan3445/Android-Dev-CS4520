10cef0c0df601876d19f580c2ea89044c30101cd
Aidan Weinberg <aidan3445@gmail.com>
2024-03-25 23:31:16 -0400

    worker time updated

--- Crafting-With-Compose/app/src/main/java/com/cs4520/assignment5/workManager/ProductWorkScheduler.kt
+++ Crafting-With-Compose/app/src/main/java/com/cs4520/assignment5/workManager/ProductWorkScheduler.kt
@@ -3,17 +3,19 @@ package com.cs4520.assignment5.workManager
 import android.content.Context
 import android.util.Log
 import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
+import java.util.concurrent.TimeUnit.HOURS
 import java.util.concurrent.TimeUnit.MINUTES
 
 class ProductWorkScheduler {
     fun scheduleProductWork(context: Context) {
         val workRequest =
             PeriodicWorkRequestBuilder<ProductRefreshWorker>(
-                15,
-                MINUTES,
+                1,
+                HOURS,
                 5,
                 MINUTES,
             )
@@ -25,7 +27,11 @@ class ProductWorkScheduler {
                 .build()
 
         // enqueue work
-        WorkManager.getInstance(context).enqueue(workRequest)
+        WorkManager.getInstance(context).enqueueUniquePeriodicWork(
+            "ProductRefreshWorker",
+            ExistingPeriodicWorkPolicy.CANCEL_AND_REENQUEUE,
+            workRequest,
+        )
 
         Log.d("Worker", "Worker scheduled")
     }
